0f667f8fd6253c307db21770eb8d444e2e1671ec
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-05-14 23:37:57 +0530

    h,m=map(int,input().split(":")) a=abs(h*30-(11*m)/2.0) if(a<360-a):     if(a>int(a)):         print(a)     else:         print(a) else:     if((360-a)>(int)(360-a)):         print(360-a)     else:         print(360-a)

---
+++ Angle_Between_Hands.py
@@ -0,0 +1,12 @@
+h,m=map(int,input().split(":"))
+a=abs(h*30-(11*m)/2.0)
+if(a<360-a):
+    if(a>int(a)):
+        print(a)
+    else:
+        print(a)
+else:
+    if((360-a)>(int)(360-a)):
+        print(360-a)
+    else:
+        print(360-a)
