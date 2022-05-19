3365998407a564032c472db37bfe3811a1a0aec6
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-05-19 23:22:46 +0530

    import string def ispangram(str):    a= "abcdefghijklmnopqrstuvwxyz"    for char in a:        if char not in str.lower():            return False    return True n= input() if(ispangram(n) == True):    print("True") else:    print("False")

---
+++ Pangram_or_not.py
@@ -0,0 +1,14 @@
+import string
+def ispangram(str):
+   a= "abcdefghijklmnopqrstuvwxyz"
+   for char in a:
+       if char not in str.lower():
+           return False
+   return True
+n= input()
+if(ispangram(n) == True):
+   print("True")
+else:
+   print("False")
+   
+  
