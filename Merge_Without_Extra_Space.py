b390423abaecb021737cc970f4d537f72205e82f
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-05-27 21:07:38 +0530

    t=int(input()) while(t>0):     n,m=map(int,input().split())     arr=list(map(int,input().split()))     arr1=list(map(int,input().split()))     res=0     res=arr+arr1     print(*(sorted(res)))     t=t-1

---
+++ Merge_Without_Extra_Space.py
@@ -0,0 +1,9 @@
+t=int(input())
+while(t>0):
+    n,m=map(int,input().split())
+    arr=list(map(int,input().split()))
+    arr1=list(map(int,input().split()))
+    res=0
+    res=arr+arr1
+    print(*(sorted(res)))
+    t=t-1
