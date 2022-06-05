b2eff1b2c8ec6e742cf4dae5c7f1e500d8070852
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-06-05 12:58:26 +0530

    from math import sqrt def is_prime(n):     if n==1:         return 0     for i in range(2,int(sqrt(n))+1):         if (n%i==0):             return 0     return 1 k=int(input()) p=int(input()) c=0 for i in range(k,p+1):     if(is_prime(i)):         c+=1 print(c)

---
+++ Primes_in_range.py
@@ -0,0 +1,16 @@
+from math import sqrt
+def is_prime(n):
+    if n==1:
+        return 0
+    for i in range(2,int(sqrt(n))+1):
+        if (n%i==0):
+            return 0
+    return 1
+k=int(input())
+p=int(input())
+c=0
+for i in range(k,p+1):
+    if(is_prime(i)):
+        c+=1
+print(c)
+    
