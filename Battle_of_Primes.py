38569f01f148283fccad00892e8dab43e3d29cbc
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-06-27 22:37:44 +0530

    n=int(input()) m=int(input()) c=2 z=0 res=n+m for i in range(1,10):     res1=res+i     for j in range(2,res1):         if(res1%j==0):             c=c+1             break     else:         print(i)         break

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,12 +1,14 @@
 n=int(input())
 m=int(input())
-r=n+m
-for i in range(1,11):
-    s2=r+i
-    for j in range(2,s2):
-        if(s2%j==0):
+c=2
+z=0
+res=n+m
+for i in range(1,10):
+    res1=res+i
+    for j in range(2,res1):
+        if(res1%j==0):
+            c=c+1
             break
     else:
-        print(i)
+        print(i) 
         break
-        
