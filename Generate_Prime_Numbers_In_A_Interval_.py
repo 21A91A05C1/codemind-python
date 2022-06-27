e0b568547ae32936c9dd818199b77685d6c9acab
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-06-27 22:39:19 +0530

    n=int(input()) m=int(input()) c=0 for i in range(n,m+1):     c=0     if(i==1):         continue     for j in range(2,i):         if(i%j==0):             c=c+1     if(c==0):         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,10 +1,12 @@
-m = int(input())
-n= int(input())
-for i in range(m,n + 1):
-   # all prime numbers are greater than 1
-   if i > 1:
-       for j in range(2,i):
-           if (i % j) == 0:
-               break
-       else:
-           print(i)
+n=int(input())
+m=int(input())
+c=0
+for i in range(n,m+1):
+    c=0
+    if(i==1):
+        continue
+    for j in range(2,i):
+        if(i%j==0):
+            c=c+1
+    if(c==0):
+        print(i)
