bcbd660c1f7ce97361796df6da4d10f8a2edff65
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-06-27 23:46:27 +0530

    n=int(input()) l=list(map(int,input().split())) max=9999 c=0 for i in range(len(l)):     if(len(str(l[i]))<max):         max=len(str(l[i])) for i in range(len(l)):     if(max==len(str(l[i]))):         c=c+1 print(c)

--- Minimum_digit_elements.py
+++ Minimum_digit_elements.py
@@ -1,11 +1,11 @@
 n=int(input())
 l=list(map(int,input().split()))
-min=999
+max=9999
 c=0
 for i in range(len(l)):
-    if len(str(l[i]))<min:
-        min=len(str(l[i]))
+    if(len(str(l[i]))<max):
+        max=len(str(l[i]))
 for i in range(len(l)):
-    if len(str(l[i]))==min:
-        c+=1
+    if(max==len(str(l[i]))):
+        c=c+1
 print(c)
