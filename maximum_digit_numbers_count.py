0fe1bd711e017fe385d5f77a16c1c2f567562e3b
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-06-27 23:57:19 +0530

    n=int(input()) l=list(map(int,input().split())) max=0 for i in range(len(l)):     if(len(str(l[i]))>max):         max=len(str(l[i])) for i in range(len(l)):     if(max==len(str(l[i]))):         print(l[i],end=" ")

--- maximum_digit_numbers_count.py
+++ maximum_digit_numbers_count.py
@@ -2,8 +2,8 @@ n=int(input())
 l=list(map(int,input().split()))
 max=0
 for i in range(len(l)):
-    if len(str(l[i]))>max:
+    if(len(str(l[i]))>max):
         max=len(str(l[i]))
 for i in range(len(l)):
-    if len(str(l[i]))==max:
+    if(max==len(str(l[i]))):
         print(l[i],end=" ")
