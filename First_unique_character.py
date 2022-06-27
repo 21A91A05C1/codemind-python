36ad1315ca04238f1061bc43efd775c59d27d653
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-06-27 22:42:50 +0530

    n=input() k=[] for i in range (len(n)):     flag=0     for j in range(len(n)):         if (n[i]==n[j] and i!=j):             flag=1             break     else:         k.append(n[i])         print(*k)         break; if(flag==1):     print("-1")

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,14 +1,15 @@
-s=input()
+n=input()
 k=[]
-for i in range(0,len(s)):
+for i in range (len(n)):
     flag=0
-    for j in range(0,len(s)):
-        if(s[i]==s[j] and i!=j):
+    for j in range(len(n)):
+        if (n[i]==n[j] and i!=j):
             flag=1
             break
     else:
-        k.append(s[i])
-        print(*k)
-        break
-if(len(k)==0):
+        k.append(n[i])
+        print(*k) 
+        break;
+if(flag==1):
     print("-1")
+        
