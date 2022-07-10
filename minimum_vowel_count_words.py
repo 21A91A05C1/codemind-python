501c17e1cc7d1bbf83151d193add28ee29edce9c
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-07-10 14:28:53 +0530

    n=list(map(str,input().split())) #print(n) k="aeiouAEIOU" u=[] d=1 min=100 for i in n:     c=0     for j in i:         if j in k:             c+=1     u.append(c)     if(c<min):         min=c     elif(c==min):         d+=1 print(d)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,25 @@
+n=list(map(str,input().split()))
+#print(n)
+k="aeiouAEIOU"
+u=[]
+d=1
+min=100
+for i in n:
+    c=0
+    for j in i:
+        if j in k:
+            c+=1
+    u.append(c)
+    if(c<min):
+        min=c
+    elif(c==min):
+        d+=1
+print(d)
+
+
+
+
+
+
+
+
