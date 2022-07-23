58a9e2732fe63a561ab84846ed0b79460213a599
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-07-23 22:40:19 +0530

    s=list(map(str,input().lower().split())) p=list(map(str,input().lower().split())) c=0 for i in s:     if i in p:         c+=1 print(c)

--- common_words_-1.py
+++ common_words_-1.py
@@ -1,7 +1,7 @@
-l=list(map(str,input().lower().split()))
-m=list(map(str,input().lower().split()))
+s=list(map(str,input().lower().split()))
+p=list(map(str,input().lower().split()))
 c=0
-for i in l:
-    if i in m:
+for i in s:
+    if i in p:
         c+=1
 print(c)
