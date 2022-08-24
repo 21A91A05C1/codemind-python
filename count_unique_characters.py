0ec067e08eb8806c823d9a35d07d7f3ad0086cf2
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-08-24 16:43:19 +0530

    n=input().lower() n=n.replace(" ","")#should keep hidden is failing otherwise c=0 for i in n:     if n.count(i)==1:         c+=1 print(c)

--- count_unique_characters.py
+++ count_unique_characters.py
@@ -1,14 +1,7 @@
-n=(input()).lower()
+n=input().lower()
+n=n.replace(" ","")#should keep hidden is failing otherwise
 c=0
-d=0
 for i in n:
-    c=0
-    if i==" ":
-        continue
-    else:
-        for j in n:
-            if i==j :
-                c+=1
-        if(c==1):
-            d+=1
-print(d)
+    if n.count(i)==1:
+        c+=1
+print(c)
