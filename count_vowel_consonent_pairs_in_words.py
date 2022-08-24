c3451c4bd4e18692d5745164e7cfd904a1e3c035
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-08-25 01:06:17 +0530

    n=input().lower().split(" ") c=0 p="aeiou" for i in n:     l=0     k=len(i)-1     while l<k:             if((i[l] in p and i[k] not  in p ) or (i[l] not  in p and i[k] in p )):                 c+=1             l+=1             k-=1 print(c)

---
+++ count_vowel_consonent_pairs_in_words.py
@@ -0,0 +1,14 @@
+n=input().lower().split(" ")
+c=0
+p="aeiou"
+for i in n:
+    l=0
+    k=len(i)-1
+    while l<k:
+            if((i[l] in p and i[k] not  in p ) or (i[l] not  in p and i[k] in p )):
+                c+=1
+            l+=1
+            k-=1
+print(c)
+    
+
