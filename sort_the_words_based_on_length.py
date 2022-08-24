3c32e9f485162051e69c5f6d734b4d733ea6a2f2
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-08-24 23:49:01 +0530

    n=list(map(str,input().split()))
    for i in range(len(n)):
        for j in range(0,len(n)):
            if(len(n[i])<len(n[j]) and i!=j):
                temp=n[i]
                n[i]=n[j]
                n[j]=temp
    print( *n)

--- sort_the_words_based_on_length.py
+++ sort_the_words_based_on_length.py
@@ -1,8 +1,11 @@
 n=list(map(str,input().split()))
-for i in range(0,len(n)):
-    for j in range (0,len(n)):
-        if i!=j and len(n[i])<len(n[j]):
+
+for i in range(len(n)):
+    for j in range(0,len(n)):
+        if(len(n[i])<len(n[j]) and i!=j):
             temp=n[i]
             n[i]=n[j]
             n[j]=temp
-print(*n)
+print( *n)
+
+
