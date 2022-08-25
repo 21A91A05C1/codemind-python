efbdd30f1274147fa15005777fc7f0d9349995ce
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-08-25 18:56:35 +0530

    n=input().lower() l=[] for i in n:     if n.count(i)==1:         if i!=" ":             l.append(i) l=sorted(l) print("".join(l))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,15 +1,8 @@
 n=input().lower()
-s=''
-c=0
+l=[]
 for i in n:
-    if(n.count(i)==1):
-        s=s+i
-s=list(s)
-s.sort()
-s=str(s)
-s=s.replace(" ","")
-s=s.replace("'","")
-s=s.replace(",","")
-s=s.replace("[","")
-s=s.replace("]","")
-print(s)
+    if n.count(i)==1:
+        if i!=" ":
+            l.append(i)
+l=sorted(l)
+print("".join(l))
