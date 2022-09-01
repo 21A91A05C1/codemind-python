78dc065bf9d95a41c513562974433d716e60a966
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-09-01 23:30:00 +0530

    n1=input().split(' ') n="" #print(n1) for i in n1:     n=n+i #print(n) print(min(n),n.count(min(n)),max(n),n.count(max(n)))

--- small_and_large_elements_appearences.py
+++ small_and_large_elements_appearences.py
@@ -1,16 +1,7 @@
-x = list(map(str,input().split()))
-b=[]
-c=[]
-for i in x:
-    n = max(i)
-    b.append(n)
-    m = min(i)
-    b.append(m)
-    for j in i:
-        c.append(j)
-a = min(b)
-d = max(b)
-print(a,end=" ")
-print(c.count(a),end=" ")
-print(d,end=" ")
-print(c.count(d),end=" ")
+n1=input().split(' ')
+n=""
+#print(n1)
+for i in n1:
+    n=n+i
+#print(n)
+print(min(n),n.count(min(n)),max(n),n.count(max(n)))
