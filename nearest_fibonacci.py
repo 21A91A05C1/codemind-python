fd9ae40b973d4ec2b6a091db427f22591748ce19
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-09-15 21:53:26 +0530

    n=int(input()) a=0 b=1 k=[] l=[] i=0 max=999 maxx=0 k.append(a) while(i!=n):     c=a+b     a=b     b=c     k.append(c)     i+=1 #print(k) for i in range(len(k)):     if(abs(k[i]-n)<max):         max=abs(k[i]-n)         maxx=k[i]         #print(max,end=" ")         #print(maxx)     elif(abs(k[i]-n)==max):         l.append(k[i]) print(maxx,*l)

---
+++ nearest_fibonacci.py
@@ -0,0 +1,27 @@
+n=int(input())
+a=0
+b=1
+k=[]
+l=[]
+i=0
+max=999
+maxx=0
+k.append(a)
+while(i!=n):
+    c=a+b
+    a=b
+    b=c
+    k.append(c)
+    i+=1
+#print(k)
+for i in range(len(k)):
+    if(abs(k[i]-n)<max):
+        max=abs(k[i]-n)
+        maxx=k[i]
+        #print(max,end=" ")
+        #print(maxx)
+    elif(abs(k[i]-n)==max):
+        l.append(k[i])
+print(maxx,*l)
+
+        
