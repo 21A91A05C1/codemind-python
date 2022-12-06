8227efd4091765df88a78610a0c19ba9b319603c
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-12-06 22:46:21 +0530

    n,m=map(int,input().split()) arr=list(map(int,input().split())) brr=list(map(int,input().split())) c=0 for i in brr:     if i in arr:         arr.remove(i)     else:        c+=1        break if(c==0):     print("Yes") else:     print("No")

--- Pasta.py
+++ Pasta.py
@@ -1,13 +1,14 @@
 n,m=map(int,input().split())
-a=list(map(int,input().split()))
-b=list(map(int,input().split()))
+arr=list(map(int,input().split()))
+brr=list(map(int,input().split()))
 c=0
-for i in b:
-    if i in a:
-        if b.count(i)<=a.count(i):
-            c+=1
-if(c==m):
+for i in brr:
+    if i in arr:
+        arr.remove(i)
+    else:
+       c+=1
+       break
+if(c==0):
     print("Yes")
 else:
     print("No")
-    
